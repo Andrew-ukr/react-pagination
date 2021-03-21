c83cc316190d4f6be2c4fe277b5fe318395494ff
Andrii <pashulyaand@gmail.com>
2021-03-21 13:27:00 +0200

    project done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,77 @@
-import React, { useState, useEffect } from 'react'
-import { useFetch } from './useFetch'
-import Follower from './Follower'
+import React, { useState, useEffect } from "react";
+import { useFetch } from "./useFetch";
+import Follower from "./Follower";
+
 function App() {
-  return <h2>pagination starter</h2>
+  const { loading, data } = useFetch();
+  const [page, setPage] = useState(0);
+  const [dataBase, setDataBase] = useState([]);
+
+  useEffect(() => {
+    setDataBase(data[page]);
+  }, [data, page]);
+
+const handlePagination = (e) => {
+  if (e.target.dataset.name === "next") {
+    if (page === data.length - 1) {
+      setPage(0);
+    } else {
+      setPage(page + 1);
+    }
+  }
+  if (e.target.dataset.name === "prev") {
+        if (page === 0) {
+          setPage(data.length - 1);
+        } else {
+          setPage(page - 1);
+        }
+  }
+  window.scrollTo(0, 0);
 }
 
-export default App
+  return (
+    <main>
+      <div className="section-title">
+        <h1>{loading ? "loading ..." : "pagination"}</h1>
+        <div className="underline"></div>
+      </div>
+      <section className="followers">
+        <div className="container">
+          {dataBase &&
+            dataBase.map((item) => (
+              <Follower key={item.id} {...item}></Follower>
+            ))}
+        </div>
+        {!loading && (
+          <div className="btn-container">
+            <button
+              className="prev-btn"
+              data-name="prev"
+              onClick={(e) => handlePagination(e)}
+            >
+              prev
+            </button>
+            {data.map((item, i) => (
+              <button
+                key={i}
+                className={i === page ? "page-btn active-btn" : "page-btn"}
+                onClick={() => {setPage(i); window.scrollTo(0, 0);}}
+              >
+                {i + 1}
+              </button>
+            ))}
+            <button
+              className="next-btn"
+              data-name="next"
+              onClick={(e) => handlePagination(e)}
+            >
+              next
+            </button>
+          </div>
+        )}
+      </section>
+    </main>
+  );
+}
+
+export default App;
--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,13 @@
-const paginate = () => {}
+const paginate = (data) => {
+  const visibleItem = 16;
+  const paginationArray = Math.ceil(data.length / visibleItem);
 
-export default paginate
+  const newArr = Array.from({ length: paginationArray }, (item, i) => {
+    const start = i * visibleItem;
+    return data.slice(start, start + visibleItem);
+  });
+
+  return newArr;
+};
+
+export default paginate;
